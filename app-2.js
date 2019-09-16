84138227301066ff4f33c5f81a6e816b70bc6c5d
Erol Bakir <erolbakir@gmail.com>
2019-09-16 14:14:28 +0200

    feat: init commit

---
+++ app-2.js
@@ -0,0 +1,62 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true);
+
+  xhr.onload = function(){
+    if(this.status === 200){
+      // console.log(this.responseText);
+
+      const costumer = JSON.parse(this.responseText);
+
+      const output = `
+      <ul>
+        <li>ID: ${costumer.id}</li>
+        <li>ID: ${costumer.name}</li>
+        <li>ID: ${costumer.company}</li>
+        <li>ID: ${costumer.phone}</li>
+      </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
+
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      // console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+      let output = '';
+
+      customers.forEach(function (costumer){
+        output += `
+          <ul>
+            <li>ID: ${costumer.id}</li>
+            <li>ID: ${costumer.name}</li>
+            <li>ID: ${costumer.company}</li>
+            <li>ID: ${costumer.phone}</li>
+          </ul>
+        `;
+      });
+
+      
+
+      document.getElementById('customers').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
